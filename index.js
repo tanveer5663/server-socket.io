18f4578b8a3e714dae1c5c88f592bd2bef00431b
Tanveer <tanveeransari5663@gmail.com>
2026-02-25 13:48:04 +0530

    fix

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ import cors from "cors";
 const app = express();
 const server = createServer(app);
 app.use(cors());
-const io = new Server(server, {
-  cors: {
-    origin: "*", // allow all origins
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  },
-});
-io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*", // allow all origins
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   },
+// });
+// io.on("connection", (socket) => {
+//   console.log("A user connected:", socket.id);
 
-  // Listen for message from client
-  socket.on("chatMessage", (msg) => {
-    console.log("Message received:", msg);
+//   // Listen for message from client
+//   socket.on("chatMessage", (msg) => {
+//     console.log("Message received:", msg);
 
-    // Send message back to all connected clients
-    io.emit("chatMessage", msg);
-  });
+//     // Send message back to all connected clients
+//     io.emit("chatMessage", msg);
+//   });
 
-  // When client disconnects
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
-});
+//   // When client disconnects
+//   socket.on("disconnect", () => {
+//     console.log("User disconnected:", socket.id);
+//   });
+// });
 app.get(
   "/",
   asyncHandler(async (req, res) => {
@@ -42,10 +42,33 @@ app.get(
   }),
 );
 
+app.get(
+  "/api/server-event",
+  asyncHandler(async (req, res) => {
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+
+    res.write("data: Connected to SSE server\n\n");
+
+    const intervalId = setInterval(() => {
+      const data = {
+        time: "tanveer"
+      };
+
+      res.write(`data: ${JSON.stringify(data)}\n\n`);
+    }, 2000);
+    req.on("close", () => {
+      console.log("Client disconnected");
+      clearInterval(intervalId);
+      res.end();
+    });
+  }),
+);
 app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message || "Something went wrong!" });
 });
 
-server.listen(3050, () => {
-  console.log("Server is running on port 3050");
+server.listen(9000, () => {
+  console.log("Server is running on port 9000");
 });
